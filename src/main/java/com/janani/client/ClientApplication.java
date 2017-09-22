49e9c2ccc45255e58227617a23074b8730f94cc2
Janani <jananise@gmail.com>
2017-09-22 16:06:56 +0530

    first commit

---
+++ src/main/java/com/janani/client/ClientApplication.java
@@ -0,0 +1,42 @@
+package com.janani.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class ClientApplication {
+
+	@Autowired
+	private EurekaClient client;
+
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
+
+	@RequestMapping("/")
+	public String callService() {
+
+		InstanceInfo instanceInfo = client.getNextServerFromEureka("service", false);
+		String baseUrl = instanceInfo.getHomePageUrl();
+		
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
+		return response.getBody();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
